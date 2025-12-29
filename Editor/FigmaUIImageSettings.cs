44a9ba6b8f1752e88d237e028d4292412f0a1230
Oleg Frolov <frolololeg@gmail.com>
2025-12-29 01:07:02 +0000

    added a settings panel to enter and save a figma token

---
+++ Editor/FigmaUIImageSettings.cs
@@ -0,0 +1,60 @@
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Volorf.FigmaUIImage
+{
+    public class FigmaUIImageSettings : EditorWindow
+    {
+        VisualTreeAsset _ui;
+        static FigmaUIImageSettings _wnd;
+        TextField _field;
+        const string FigmaTokenKeyName = "FIGMA_TOKEN";
+
+        [MenuItem("Tools/Figma UI Image/Settings", false, 1)]
+        public static void ShowSettings()
+        {
+            _wnd = GetWindow<FigmaUIImageSettings>();
+            _wnd.titleContent = new GUIContent("Figma UI Image Settings");
+        }
+
+        [MenuItem("Tools/Figma UI Image/Update All", false, 2)]
+        public static void UpdateAll()
+        {
+            IFigmaImageUpdatable[] figmaImageUpdatables =
+                FindObjectsOfType<MonoBehaviour>(false).OfType<IFigmaImageUpdatable>().ToArray();
+
+            foreach (IFigmaImageUpdatable updatable in figmaImageUpdatables)
+            {
+                updatable.UpdateFigmaImage();
+            }
+
+            Debug.Log("All textures have been updated.");
+        }
+
+        public void CreateGUI()
+        {
+            _ui = Resources.Load<VisualTreeAsset>("FigmaUIImageSettings");
+            VisualElement root = rootVisualElement;
+            VisualElement labelFromUXML = _ui.Instantiate();
+            root.Add(labelFromUXML);
+
+            Button apply = root.Q<Button>("Apply");
+            _field = root.Q<TextField>("Token");
+            _field.value = PlayerPrefs.GetString(FigmaTokenKeyName);
+
+            apply.RegisterCallback<ClickEvent>(SaveFigmaToken);
+        }
+
+        void SaveFigmaToken(ClickEvent ev)
+        {
+            PlayerPrefs.SetString(FigmaTokenKeyName, _field.value);
+            _wnd.Close();
+            #if UNITY_EDITOR
+            Debug.Log($"Figma UI Image Token was saved locally: {_field.value}");
+            #endif
+        }
+
+    }
+}
